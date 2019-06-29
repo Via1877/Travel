999faced91cb3dd9b1a1303955892d5e74f7123e
annie <amy_603@126.com>
2019-06-29 17:22:53 +0800

    add weekend

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="index-recommend">
-    <div class="recommend-title">热销推荐</div>
+    <div class="recommend-title">周末去哪儿</div>
     <div class="recommend border-bottom"  v-for="item in iconList" :key="item.id" >
       <div class="recommend-img" >
         <img class="recommend-img-content"  :src="item.src">
@@ -8,7 +8,6 @@
       <div class="recommend-img-info">
         <p class="recommend-img-title">{{item.title}}</p>
         <p class="recommend-img-desc">{{item.info}}</p>
-        <button class="read-more">查看详情</button>
       </div>
     </div>
   </div>
@@ -16,44 +15,29 @@
 
 <script>
 export default {
-  name: 'HomeRecommend',
+  name: 'HomeWeekend',
   data () {
     return {
             iconList: [{
               id: '001',
-              src: 'https://imgs.qunarzz.com/p/tts6/1801/82/c63867fd83805502.jpg_r_390x260x90_de91e096.jpg',
+              src: 'https://imgs.qunarzz.com/vs_ceph_vs_tts/5e1cb631-d03e-4dca-90ab-ee9e08fa3c0e.jpg_r_390x260x90_f289407a.jpg',
               title: '北京-丽江6天5晚跟团游',
               info: '升碧桂园别墅🔥张家界+玻璃桥+天门山凤凰丨家庭亲子双飞6日｜头等航空舱座椅'
             }, {
               id: '002',
-              src: 'https://img1.qunarzz.com/vs_ceph_vs_tts/ade31913-f43a-4fbb-a480-299e314a1b1a.jpg_r_240x160x90_12bdb5de.jpg',
+              src: 'https://imgs.qunarzz.com/vs_ceph_vs_tts/5e1cb631-d03e-4dca-90ab-ee9e08fa3c0e.jpg_r_390x260x90_f289407a.jpg',
               title: '北京-泰国6天5晚跟团游',
               info: '升碧桂园别墅🔥张家界+玻璃桥+天门山凤凰丨家庭亲子双飞6日｜头等航空舱座椅'
             }, {
               id: '003',
-              src: 'https://img1.qunarzz.com/p/tts0/1801/ce/980d870a80048602.jpg_r_240x160x90_0d916950.jpg',
+              src: 'https://imgs.qunarzz.com/vs_ceph_vs_tts/5e1cb631-d03e-4dca-90ab-ee9e08fa3c0e.jpg_r_390x260x90_f289407a.jpg',
               title: '北京-户部巷3天2晚自由行',
-              info: '升碧桂园别墅🔥张家界+玻璃桥+天门山凤凰丨家庭亲子双飞6日｜头等航空舱座椅'
-            }, {
-              id: '004',
-              src: 'https://img1.qunarzz.com/vs_ceph_vs_tts/ade31913-f43a-4fbb-a480-299e314a1b1a.jpg_r_240x160x90_12bdb5de.jpg',
-              title: '北京-泰国6天5晚跟团游',
-              info: '升碧桂园别墅🔥张家界+玻璃桥+天门山凤凰丨家庭亲子双飞6日｜头等航空舱座椅'
-            }, {
-              id: '005',
-              src: 'https://img1.qunarzz.com/p/tts0/1801/ce/980d870a80048602.jpg_r_240x160x90_0d916950.jpg',
-              title: '北京-户部巷3天2晚自由行',
-              info: '升碧桂园别墅🔥张家界+玻璃桥+天门山凤凰丨家庭亲子双飞6日｜头等航空舱座椅'
-            }, {
-              id: '006',
-              src: 'https://imgs.qunarzz.com/p/tts6/1801/82/c63867fd83805502.jpg_r_390x260x90_de91e096.jpg',
-              title: '北京-丽江6天5晚跟团游',
               info: '升碧桂园别墅🔥张家界+玻璃桥+天门山凤凰丨家庭亲子双飞6日｜头等航空舱座椅'
             }
           ]
+      }
     }
   }
-}
 </script>
 
 <style lang="stylus" scoped>
@@ -68,28 +52,19 @@ export default {
       background: #eee
       padding-left: .24rem
     .recommend
-      display: flex
-      width: 100%
-      height: 0
-      padding-bottom: 25.5%
-      display: flex
       .recommend-img
-        width: 35%
-        height: 100%
-        padding: .1rem
-        vertical-align: middle
+        width: 100%
+        padding-bottom: 66.7%
+        height: 0
+        overflow: hidden
         .recommend-img-content
           width: 100%
       .recommend-img-info
-        flex: 1
-        padding-left: .1%
-        min-width: 0
-        padding-top: .1rem
-        vertical-align: middle
+        padding: .1rem
         .recommend-img-title
           line-height: .54rem
           font-size: .32rem
-          font-weight: blod
+          font-weight: bold
           ellipsis ()
         .recommend-img-desc 
           color: $darkTextColor
